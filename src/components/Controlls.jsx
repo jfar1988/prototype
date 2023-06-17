0418c57a011111cc9e968a20a9e4810e25f1ae6e
jfar1988 <jfar1988@misena.edu.co>
2023-06-17 15:54:22 -0500

    Subiendo el link del repositorio de github

--- src/components/Controlls.jsx
+++ src/components/Controlls.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Controlls = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-2 px-4 text-center">
+    <footer className="fixed bottom-0 right-0 bg-gray-200 py-2 px-4 text-center flex items-center justify-end">
       <p className="text-sm">
         Press:
         <span className="font-bold"> W </span>forward,
@@ -11,6 +13,15 @@ const Controlls = () => {
         <span className="font-bold"> D </span>right,
         <span className="font-bold"> SHIFT </span>speed
       </p>
+      <a
+        href="https://github.com/jfar1988/prototype"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-gray-500 hover:text-gray-700 flex items-center ml-4"
+      >
+        <FontAwesomeIcon icon={faGithub} className="mr-1" />
+        Repositorio en GitHub
+      </a>
     </footer>
   );
 };
